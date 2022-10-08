21fa55d4547d970f3e6a1cdb1328d70bd4d8add4
Tiago de Freitas <tiago.freitas2@fatec.sp.gov.br>
2022-10-07 23:24:02 -0300

    Adicionado mais eXercícioos

---
+++ tarefas.php
@@ -0,0 +1,19 @@
+<html>
+    <head>
+        <title>Gerenciador de Tarefas</title>
+    </head>
+    <body>
+        <h1>Gerenciador de Tarefas</h1>
+        <!-- Aqui irá o restante do código -->
+        <form>
+            <fieldset>
+                <legend>Nova tarefa</legend>
+            <label>
+                Tarefa:
+                <input type="text" name="nome" />
+            </label>
+                <input type="submit" value="Cadastrar" />
+            </fieldset>
+        </form>
+    </body>
+</html>
